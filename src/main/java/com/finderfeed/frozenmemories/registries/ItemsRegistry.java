8580321be8958f1cd72095bf737b04824cf52732
FINDERFEED <windingaster335@gmail.com>
2021-12-07 22:07:47 +0300

    LoreProgram

--- src/main/java/com/finderfeed/frozenmemories/registries/ItemsRegistry.java
+++ src/main/java/com/finderfeed/frozenmemories/registries/ItemsRegistry.java
@@ -9,13 +9,15 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class ItemsRegistry {
 
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FrozenMemories.MOD_ID);
 
     public static final RegistryObject<BlockItem> LORE_TILE_BLOCK = registerBlockItem(new Item.Properties().tab(FrozenMemories.FROZEN_MEMORIES)
-            ,BlocksRegistry.LORE_TILE_BLOCK.get(),"lore_tile_block");
+            ,()->BlocksRegistry.LORE_TILE_BLOCK.get(),"lore_tile_block");
 
     public static final RegistryObject<HelperWand> HELPER_WAND = registerItem(new HelperWand(),"helper_wand");
 
@@ -24,8 +26,8 @@ public class ItemsRegistry {
         return ITEMS.register(name,()->item);
     }
 
-    public static RegistryObject<BlockItem> registerBlockItem(Item.Properties props,Block block, String name){
-        return ITEMS.register(name,()->new BlockItem(block,props));
+    public static RegistryObject<BlockItem> registerBlockItem(Item.Properties props, Supplier<Block> block, String name){
+        return ITEMS.register(name,()->new BlockItem(block.get(),props));
     }
 
 }
